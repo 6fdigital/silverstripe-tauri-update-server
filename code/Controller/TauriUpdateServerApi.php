2461e62cc37112e45682aae105699e4b2bfa1139
Marco Kernler <marco.kernler@6f.digital>
2022-07-06 15:16:35 +0200

    #2p6hn98 - Cleanup

--- code/Controller/TauriUpdateServerApi.php
+++ code/Controller/TauriUpdateServerApi.php
@@ -33,20 +33,22 @@ class TauriUpdateServerApi extends Controller
     "exe"
   ];
 
-  public function index() {
-    return[];
+  public function index()
+  {
+    return [];
   }
 
   /**
    * @return false|string
    * @throws \SilverStripe\ORM\ValidationException
    */
-  public function add() {
+  public function add()
+  {
     //
     $request = $this->getRequest();
     $response = $this->getResponse();
-    $manifestData = $this->getRequest()->postVar("MANIFEST");
-    $token = $this->getRequest()->postVar("TOKEN");
+    $manifestData = $request->postVar("MANIFEST");
+    $token = $request->postVar("TOKEN");
 
     //
     if (!$manifestData) {
@@ -68,87 +70,36 @@ class TauriUpdateServerApi extends Controller
         ]
     }
      */
+    // try parsing the given manifest
     $manifest = new TUSReleaseManifest();
     if (!$manifest->parse($manifestData)) {
       return $this->_respond("Could not parse manifest", 400);
     }
 
-    /*
-    Debug::dump($_FILES["ARTIFACT_DARWIN"]);
-
-    //
-    if(
-        (!array_key_exists("ARTIFACT_DARWIN", $_FILES) &&
-        !array_key_exists("ARTIFACT_LINUX", $_FILES) &&
-        !array_key_exists("ARTIFACT_WINDOWS", $_FILES)) ||
-        !$dataRaw
-    ) {
-        return $this->_respond("Please specify at least one artifact!", 400);
-    }
-
-    //
-    $assetDarwin = $_FILES["ARTIFACT_DARWIN"];
-    $assetLinux = $_FILES["ARTIFACT_LINUX"];
-    $assetWindows = $_FILES["ARTIFACT_WINDOWS"];
-    $artifactFiles = [];
-
-    //
-    if ($assetDarwin["error"] !== UPLOAD_ERR_OK && $assetLinux["error"] !== UPLOAD_ERR_OK && $assetWindows["error"] !== UPLOAD_ERR_OK) {
-        return $this->_respond("Received empty files!", 400);
-    }
-
-    //
-    if (!$data = json_decode($dataRaw)) {
-        return $this->_respond("Could not parse release data!", 400);
-    }*/
-
-    //
+    // search for application
     if (!$application = Application::get()->filter(["Title" => $manifest->getApplication()])->first()) {
       return $this->_respond("Could not find application!", 400);
     }
 
-    //
+    // check whether the current user are able
+    // to create new releases
     if (!$application->canCreateRelease($token)) {
       return $this->_respond("You're not allowed to create releases!", 405);
     }
 
-    //
+    // check if new version are greater than
+    // current latest one
     if ($application->latestRelease() &&
-      !Comparator::greaterThan($manifest->getVersion(), $application->latestRelease()->Version))
-    {
+      !Comparator::greaterThan($manifest->getVersion(), $application->latestRelease()->Version)) {
       return $this->_respond(sprintf("The new version (%s) must be greater than the latest one (%s)!", $manifest->getVersion(), $application->latestRelease()->Version), 400);
     }
 
-    /*
-    //
-    if ($assetDarwin["error"] === UPLOAD_ERR_OK) {
-        if (!$darwinFile = $this->_saveAsset($assetDarwin)) {
-            return $this->_respond("Could not upload artifact!", 400);
-        }
-
-        $artifactFiles["darwin"] = $darwinFile;
-    }
-    //
-    if ($assetLinux["error"] === UPLOAD_ERR_OK) {
-        if (!$linuxFile = $this->_saveAsset($assetLinux)) {
-            return $this->_respond("Could not upload artifact!", 400);
-        }
-
-        $artifactFiles["linux"] = $linuxFile;
-    }
-    //
-    if ($assetWindows["error"] === UPLOAD_ERR_OK) {
-        if (!$windowsFile = $this->_saveAsset($assetWindows)) {
-            return $this->_respond("Could not upload artifact!", 400);
-        }
-
-        $artifactFiles["windows"] = $windowsFile;
-    }*/
-
+    // upload artifacts
     if (!$manifest->uploadArtifacts()) {
       return $this->_respond("Could not upload artifacts!", 400);
     }
 
+    // create release
     if (!$release = $manifest->createRelease($application)) {
       return $this->_respond("Could not create release", 400);
     }
@@ -156,36 +107,15 @@ class TauriUpdateServerApi extends Controller
     return $this->_respond("Release was created successfully");
   }
 
-  /*
-  protected function _saveAsset($asset): ?File {
-      //
-      $allowedUploadExtensions = Config::inst()->get(TauriUpdateServerApi::class, "allowed_upload_extensions");
-      // create a custom validator to ensure
-      // correct media type uploads
-      $uploadValidator = new Upload_Validator();
-      $uploadValidator->setAllowedExtensions($allowedUploadExtensions);
-
-      //
-      $assetFile = File::create();
-      $upload = Upload::create();
-      $upload->setValidator($uploadValidator);
-
-      //
-      $res = $upload->loadIntoFile($asset, $assetFile, "/Uploads");
-      $res1 = $assetFile->write();
-
-      // generate the thumbnails for the uploaded file
-      AssetAdmin::singleton()->generateThumbnails($assetFile);
-
-      if (!$res || !$res1) {
-          return null;
-      }
-
-      return $assetFile;
-  }*/
-
-
-  protected function _respond($message, $code = 200) {
+  /**
+   * Respond with a simple json object containing
+   * the given status-code as well as a message.
+   * @param $message
+   * @param $code
+   * @return false|string
+   */
+  protected function _respond($message, $code = 200)
+  {
     //
     $this->getResponse()->addHeader("Content-Type", "application/json");
     $this->getResponse()->setStatusCode($code);
